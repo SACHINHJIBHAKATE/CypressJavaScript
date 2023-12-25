de2a863a9798075933ed14ac61b2480dabbf770e
SACHINHJIBHAKATE <sachinjibhakate@gmail.com>
2023-12-25 03:02:31 +0100

    added Test3.js

---
+++ cypress/integration/Test3.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+describe('Test Suite',function(){
+
+    it ('Test Case',function(){
+    
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/') 
+    })
+})
